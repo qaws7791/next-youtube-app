c5f5657e92e1e5a009815efed814800b3772537e
qaws7791 <qaws7791@gmail.com>
2024-07-18 23:42:31 +0900

    feat: tags 추가

--- app/videos/[id]/page.tsx
+++ app/videos/[id]/page.tsx
@@ -1,11 +1,9 @@
 import placeholder from "@/lib/placeholder/playlistitems.json";
-// import { Link } from "next-view-transitions";
-// import Link from "next/link";
 import Image from "next/image";
 import { Metadata } from "next";
 import BackButton from "@/components/back-button";
-import Description from "@/components/description";
 import { fetchAllItemsOfPlaylist, fetchVideo } from "@/lib/google/youtube";
+import Details from "@/components/details";
 
 interface Props {
   params: { id: string };
@@ -19,12 +17,6 @@ export async function generateStaticParams() {
   return items.map((item) => ({
     id: item.snippet?.resourceId?.videoId,
   }));
-}
-
-function getVideo(id: string) {
-  return placeholder.items.find(
-    (item) => item.snippet.resourceId.videoId === id
-  );
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
@@ -55,12 +47,15 @@ export default async function VideoPage({ params: { id } }: Props) {
   }
 
   return (
-    <main className="max-w-screen-xl bg-neutral-950 mx-auto rounded-4xl sm:rounded-6xl transition-all">
+    <main className="max-w-screen-xl bg-neutral-950 mx-auto rounded-3xl sm:rounded-6xl transition-all py-4">
       <div className="m-4 sm:m-8 inline-block">
         <BackButton />
-        <h1 className="text-2xl sm:text-4xl font-bold my-8">
+        <h1 className="text-2xl sm:text-4xl font-bold my-8" id="video-title">
           {video.snippet.title}
         </h1>
+        <p className="text-lg sm:text-xl font-semibold" id="video-channel">
+          {video.snippet.channelTitle}
+        </p>
       </div>
 
       <div className="relative overflow-hidden w-full aspect-video">
@@ -70,6 +65,7 @@ export default async function VideoPage({ params: { id } }: Props) {
           src={`https://www.youtube.com/embed/${id}?autoplay=0&origin=http://localhost.com&loop=1`}
         ></iframe>
         <Image
+          id="thumbnail"
           src={thumbnail}
           alt={video.snippet.title!}
           width={thumbnailWidth}
@@ -82,9 +78,37 @@ export default async function VideoPage({ params: { id } }: Props) {
         />
       </div>
 
-      <div className="p-4 sm:p-8 py-8 sm:py-12">
-        <h2 className="text-xl sm:text-2xl font-semibold">Description</h2>
-        <Description description={video.snippet.description!} />
+      <div className="p-4 sm:p-8">
+        <h2
+          className="text-xl sm:text-2xl font-semibold"
+          id="video-description"
+        >
+          Description
+        </h2>
+        <Details
+          description={video.snippet.description!}
+          publishedAt={video.snippet.publishedAt!}
+        />
+      </div>
+
+      <div className="px-4 pb-4 sm:px-8 sm:pb-8">
+        <h2 className="text-xl sm:text-2xl font-semibold" id="video-tags">
+          Tags
+        </h2>
+        <div className="mt-4">
+          {video.snippet.tags?.map((tag, index) => (
+            <a
+              key={index}
+              className="inline-block m-1 font-bold px-3 py-1 border border-neutral-600 rounded-lg hover:bg-neutral-50/10 focus:bg-neutral-600/10"
+              href={`https://www.youtube.com/results?search_query=${tag}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label={`Search ${tag}`}
+            >
+              #{tag}
+            </a>
+          ))}
+        </div>
       </div>
     </main>
   );
