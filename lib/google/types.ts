8e61d8368dfe760e6d18b43bff38d914695497d4
qaws7791 <qaws7791@gmail.com>
2024-07-26 23:03:40 +0900

    feat: Playlist, PlaylistWithItems 타입 추가

--- lib/google/types.ts
+++ lib/google/types.ts
@@ -1,3 +1,7 @@
 import { youtube_v3 } from "@googleapis/youtube";
 
 export type PlayListItem = youtube_v3.Schema$PlaylistItem;
+
+export type Playlist = youtube_v3.Schema$Playlist;
+
+export type PlaylistWithItems = Playlist & { items: PlayListItem[] };
