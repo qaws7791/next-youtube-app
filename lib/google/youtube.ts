ccbb08901f295e5bde069f662ab34fde6aa88352
qaws7791 <qaws7791@gmail.com>
2024-07-27 23:12:24 +0900

    feat: YouTube API 캐싱

--- lib/google/youtube.ts
+++ lib/google/youtube.ts
@@ -1,16 +1,17 @@
 import { youtube } from "@/lib/google";
+import { unstable_cache } from "next/cache";
 import { cache } from "react";
 
-export const fetchYouTubePlaylist = async (playlistId: string) => {
+export const fetchYouTubePlaylist = cache(async (playlistId: string) => {
   const res = await youtube.playlists.list({
-    id: [process.env.YOUTUBE_PLAYLIST_ID!],
+    id: [playlistId],
     part: ["contentDetails", "id", "snippet", "status"],
   });
   return res.data;
-};
+});
 
-export const fetchAllItemsOfPlaylist = async (playlistId: string) => {
-  console.log("Fetching all items of playlist", playlistId);
+export const fetchAllItemsOfPlaylist = cache(async (playlistId: string) => {
+  console.log("Fetching all items of playlist: ", playlistId);
   let nextPageToken: string = "";
   const items = [];
   do {
@@ -31,34 +32,39 @@ export const fetchAllItemsOfPlaylist = async (playlistId: string) => {
       totalResults: items.length,
     },
   };
-};
+});
 
-export const fetchYoutubeVideo = async (videoId: string) => {
+export const fetchYoutubeVideo = cache(async (videoId: string) => {
+  console.log(`Fetching video: ${videoId}`);
   const res = await youtube.videos.list({
     id: [videoId],
     part: ["contentDetails", "id", "snippet", "status"],
   });
   return res.data.items?.[0];
-};
-
-export const fetchYoutubePlaylists = cache(async (playlistIds: string[]) => {
-  const playlists = await youtube.playlists.list({
-    id: playlistIds,
-    part: ["contentDetails", "id", "snippet", "status"],
-    maxResults: 50,
-  });
-
-  if (!playlists.data.items) throw new Error("No playlists found");
-
-  const itemsFetchPromises = playlists.data.items.map((playlist) => {
-    return fetchAllItemsOfPlaylist(playlist.id!);
-  });
-
-  const items = await Promise.all(itemsFetchPromises);
-  return playlists.data.items.map((playlist, index) => {
-    return {
-      ...playlist,
-      items: items[index].items,
-    };
-  });
 });
+
+export const fetchYoutubePlaylists = unstable_cache(
+  async (playlistIds: string[]) => {
+    console.log(`Fetching ${playlistIds.length} playlists`);
+    const playlists = await youtube.playlists.list({
+      id: playlistIds,
+      part: ["contentDetails", "id", "snippet", "status"],
+      maxResults: 50,
+    });
+
+    if (!playlists.data.items) throw new Error("No playlists found");
+
+    const itemsFetchPromises = playlists.data.items.map((playlist) => {
+      return fetchAllItemsOfPlaylist(playlist.id!);
+    });
+
+    const items = await Promise.all(itemsFetchPromises);
+    return playlists.data.items.map((playlist, index) => {
+      return {
+        ...playlist,
+        items: items[index].items,
+      };
+    });
+  },
+  ["fetchYoutubePlaylists"]
+);
